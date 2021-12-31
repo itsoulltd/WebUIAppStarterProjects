7157a862238e9249245045ff7ea0ef5693f2736b
Towhid Islam <m.towhid.islam@gmail.com>
2022-01-01 00:11:39 +0600

    How a client app connect to other rest-apis.

--- VueJsSpringStartup-3/src/main/java/com/infoworks/lab/webapp/config/BeanConfig.java
+++ VueJsSpringStartup-3/src/main/java/com/infoworks/lab/webapp/config/BeanConfig.java
@@ -2,10 +2,18 @@ package com.infoworks.lab.webapp.config;
 
 import com.infoworks.lab.domain.entities.Passenger;
 import com.it.soul.lab.data.simple.SimpleDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RootUriTemplateHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class BeanConfig {
 
     @Bean("HelloBean")
@@ -18,4 +26,15 @@ public class BeanConfig {
         return new SimpleDataSource<>();
     }
 
+    @Value("${app.gateway.uri}")
+    private String apiBaseUrl;
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .uriTemplateHandler(new RootUriTemplateHandler(apiBaseUrl))
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
+    }
 }
