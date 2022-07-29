8ec78d17d98ecec96f567003d6f1aeb3ba1c029d
Towhid Islam <m.towhid.islam@gmail.com>
2022-07-29 16:16:45 +0600

    vuetify2: bugfix for docker

--- VuetifySpringStartup-2/src/main/java/com/infoworks/lab/controllers/rest/FileUploadController.java
+++ VuetifySpringStartup-2/src/main/java/com/infoworks/lab/controllers/rest/FileUploadController.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/files")
+@RequestMapping("/api/files")
 public class FileUploadController {
 
     private LocalStorageService storageService;
--- VuetifySpringStartup-2/src/main/java/com/infoworks/lab/controllers/rest/MessageController.java
+++ VuetifySpringStartup-2/src/main/java/com/infoworks/lab/controllers/rest/MessageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/messages")
+@RequestMapping("/api/messages")
 public class MessageController {
 
     @GetMapping("/hello")
