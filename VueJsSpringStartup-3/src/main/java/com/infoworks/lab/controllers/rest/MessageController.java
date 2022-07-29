93f88e0c2ee9c4f18effae6f33bf806d351e7111
Towhid Islam <m.towhid.islam@gmail.com>
2022-07-29 16:17:55 +0600

    vue3: bugfix for docker

--- VueJsSpringStartup-3/src/main/java/com/infoworks/lab/controllers/rest/MessageController.java
+++ VueJsSpringStartup-3/src/main/java/com/infoworks/lab/controllers/rest/MessageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/messages")
+@RequestMapping("/api/messages")
 public class MessageController {
 
     @GetMapping("/hello")
