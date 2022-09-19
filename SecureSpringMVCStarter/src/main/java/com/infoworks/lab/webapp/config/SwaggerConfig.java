f088357f55f23335eb967f412703b9fbc9efd2e4
Towhidul Islam <m.towhid.islam@gmail.com>
2022-09-19 14:57:03 +0600

    spring-mvc: tried but issue exist. springfox-swagger-data-rest issue

--- SecureSpringMVCStarter/src/main/java/com/infoworks/lab/webapp/config/SwaggerConfig.java
+++ SecureSpringMVCStarter/src/main/java/com/infoworks/lab/webapp/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.infoworks.lab.webapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+//@Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
 public class SwaggerConfig {
 
     @Bean
