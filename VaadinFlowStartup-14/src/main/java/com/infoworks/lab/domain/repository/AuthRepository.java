bd06dab85e3f114a7f10517869dabe3fee423504
Towhid Islam <m.towhid.islam@gmail.com>
2023-01-06 18:11:15 +0600

    vaadin-14:feat: adding role checking in AuthRepository

--- VaadinFlowStartup-14/src/main/java/com/infoworks/lab/domain/repository/AuthRepository.java
+++ VaadinFlowStartup-14/src/main/java/com/infoworks/lab/domain/repository/AuthRepository.java
@@ -1,11 +1,31 @@
 package com.infoworks.lab.domain.repository;
 
+import com.infoworks.lab.jjwt.JWTPayload;
+import com.infoworks.lab.jjwt.TokenValidator;
+import com.vaadin.flow.component.UI;
+
+import java.util.Optional;
 import java.util.function.BiConsumer;
+import java.util.stream.Stream;
 
 public class AuthRepository {
 
     public static final String X_AUTH_TOKEN = "X-Auth-Token";
     public static final String X_RESET_TOKEN = "Reset-Pass-Token";
+
+    public static boolean matchAnyRole(String...anyRoles) {
+        Optional<Object> optToken = Optional.ofNullable(UI.getCurrent().getSession().getAttribute(X_AUTH_TOKEN));
+        if (optToken.isPresent()) {
+            String token = optToken.get().toString();
+            JWTPayload payload = TokenValidator.parsePayload(token, JWTPayload.class);
+            String userHasRoles = payload.getData().get("roles");
+            if (userHasRoles == null || userHasRoles.isEmpty()) return false;
+            final String userHasRolesUP = userHasRoles.toUpperCase();
+            return Stream.of(anyRoles)
+                    .anyMatch(role -> userHasRolesUP.contains(role.toUpperCase()));
+        }
+        return false;
+    }
 
     public void doLogin(String username , String password, BiConsumer<Boolean, String> consumer) {
         //TODO:
