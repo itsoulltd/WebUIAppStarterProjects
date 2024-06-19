ccc8f1ecd23a7a740f18302546feed8c17acc186
Towhid Islam <m.towhid.islam@gmail.com>
2024-06-19 12:48:23 +0200

    chore: house-keeping

--- SecureSpringMVCStarter/src/main/java/com/infoworks/lab/webapp/config/StartupConfig.java
+++ SecureSpringMVCStarter/src/main/java/com/infoworks/lab/webapp/config/StartupConfig.java
@@ -1,19 +1,10 @@
 package com.infoworks.lab.webapp.config;
 
-import com.infoworks.lab.jsql.JsqlConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartupConfig implements CommandLineRunner {
-
-    @Autowired
-    private JsqlConfig jsqlConfig;
-
-    @Autowired @Qualifier("AppDBNameKey")
-    private String dbKey;
 
     @Override
     public void run(String... args) throws Exception {
