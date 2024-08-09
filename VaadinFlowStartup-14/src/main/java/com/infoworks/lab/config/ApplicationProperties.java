8f7a601ce2950e7558fa58f4dde345eaf1089357
Towhid Islam <m.towhid.islam@gmail.com>
2024-08-09 17:12:06 +0200

    vaadin-14:fix: fixing IS_AUTH_DISABLE prop logic

--- VaadinFlowStartup-14/src/main/java/com/infoworks/lab/config/ApplicationProperties.java
+++ VaadinFlowStartup-14/src/main/java/com/infoworks/lab/config/ApplicationProperties.java
@@ -4,7 +4,13 @@ import java.util.Optional;
 
 public class ApplicationProperties {
 
-    public static Boolean IS_AUTH_DISABLE = Boolean.parseBoolean(Optional.ofNullable(System.getenv("app.auth.disable")).orElse("false"));
+    public static Boolean IS_AUTH_DISABLE = Boolean.parseBoolean(
+            Optional.ofNullable(
+                    System.getProperty("app.auth.disable") != null
+                            ? System.getProperty("app.auth.disable")
+                            : System.getenv("app.auth.disable")
+            ).orElse("false")
+    );
     public static String GOOGLE_MAP_API_KEY = Optional.ofNullable(
             System.getProperty("google.maps.api") != null
                     ? System.getProperty("google.maps.api")
