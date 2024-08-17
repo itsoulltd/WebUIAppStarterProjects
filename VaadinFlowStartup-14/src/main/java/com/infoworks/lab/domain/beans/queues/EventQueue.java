974aac86300fbed2ec389cb16988196fb09f4654
Towhid Islam <m.towhid.islam@gmail.com>
2024-08-17 10:23:38 +0200

    vaadin-14:fix: EventQueue Updated

--- VaadinFlowStartup-14/src/main/java/com/infoworks/lab/domain/beans/queues/EventQueue.java
+++ VaadinFlowStartup-14/src/main/java/com/infoworks/lab/domain/beans/queues/EventQueue.java
@@ -7,8 +7,7 @@ import com.infoworks.lab.beans.tasks.definition.TaskStack;
 import com.infoworks.lab.rest.models.Message;
 import com.vaadin.flow.component.UI;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.function.BiConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,6 +17,7 @@ public class EventQueue extends AbstractTaskQueue {
     protected static Logger LOG = Logger.getLogger(EventQueue.class.getSimpleName());
     public static final String X_EVENT_QUEUE = "X-Event-Queue";
     public static final String X_EVENT_EXECUTOR = "X-Event-Executor";
+    public static final String X_EVENT_SCHEDULER_EXECUTOR = "X-Event-Scheduler-Executor";
     public static final Integer MAX_POOL_COUNT = 10;
 
     public static void register() {
@@ -31,31 +31,40 @@ public class EventQueue extends AbstractTaskQueue {
             });
             UI.getCurrent().getSession().setAttribute(X_EVENT_QUEUE, queue);
         }
-
+        //
         Object oldE = UI.getCurrent().getSession().getAttribute(X_EVENT_EXECUTOR);
         if (oldE == null) {
             UI.getCurrent().getSession().setAttribute(X_EVENT_EXECUTOR
                     , Executors.newFixedThreadPool(MAX_POOL_COUNT));
         }
+        //
+        Object oldSE = UI.getCurrent().getSession().getAttribute(X_EVENT_SCHEDULER_EXECUTOR);
+        if (oldSE == null) {
+            UI.getCurrent().getSession().setAttribute(X_EVENT_SCHEDULER_EXECUTOR
+                    , Executors.newScheduledThreadPool(MAX_POOL_COUNT));
+        }
     }
 
     public static void unregister() {
-        Object evnQueue = UI.getCurrent().getSession().getAttribute(X_EVENT_QUEUE);
-        if (evnQueue != null && evnQueue instanceof EventQueue) {
-            ((EventQueue) evnQueue).onTaskComplete((message, state) -> {});
-        }
-        UI.getCurrent().getSession().setAttribute(X_EVENT_QUEUE, null);
-        //
-        Object oldE = UI.getCurrent().getSession().getAttribute(X_EVENT_EXECUTOR);
-        if (oldE != null) {
-            try {
-                ((ExecutorService)oldE).shutdown();
-            } catch (Exception e) {
-                LOG.log(Level.WARNING, e.getMessage());
-            } finally {
-                UI.getCurrent().getSession().setAttribute(X_EVENT_EXECUTOR, null);
+        recycle(X_EVENT_QUEUE);
+        recycle(X_EVENT_EXECUTOR);
+        recycle(X_EVENT_SCHEDULER_EXECUTOR);
+    }
+
+    private static void recycle(String attributeKey) {
+        Object exeQueue = UI.getCurrent().getSession().getAttribute(attributeKey);
+        if (exeQueue != null) {
+            if (exeQueue instanceof ExecutorService) {
+                try {
+                    ((ExecutorService)exeQueue).shutdown();
+                } catch (Exception e) {
+                    LOG.log(Level.WARNING, e.getMessage());
+                }
+            } else if (exeQueue instanceof EventQueue) {
+                ((EventQueue) exeQueue).onTaskComplete((message, state) -> {});
             }
         }
+        UI.getCurrent().getSession().setAttribute(attributeKey, null);
     }
 
     public static void dispatchTask(Task aTask) {
@@ -72,7 +81,21 @@ public class EventQueue extends AbstractTaskQueue {
         }
     }
 
-    /////////////////////Impl-EventQueue///////////////////
+    public static void dispatch(int delay, TimeUnit unit, Callable callable) {
+        if (delay > 0) {
+            Object evnQueue = UI.getCurrent().getSession().getAttribute(X_EVENT_SCHEDULER_EXECUTOR);
+            if (evnQueue != null && evnQueue instanceof ScheduledExecutorService) {
+                ((ScheduledExecutorService) evnQueue).schedule(callable, delay, unit);
+            }
+        } else {
+            Object evnQueue = UI.getCurrent().getSession().getAttribute(X_EVENT_EXECUTOR);
+            if (evnQueue != null && evnQueue instanceof ExecutorService) {
+                ((ExecutorService) evnQueue).submit(callable);
+            }
+        }
+    }
+
+    //////////////////////////////////Impl-EventQueue/////////////////////////////////
 
     private final TaskQueue exeQueue;
 
@@ -110,5 +133,5 @@ public class EventQueue extends AbstractTaskQueue {
         return this;
     }
 
-    /////////////////////End-EventQueue///////////////////
+    //////////////////////////////////End-EventQueue////////////////////////////////
 }
