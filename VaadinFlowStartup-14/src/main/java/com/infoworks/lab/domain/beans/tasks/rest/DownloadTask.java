bd614ce86a9d404fdf4cbc4a86517e1dba29c25b
Towhid Islam <m.towhid.islam@gmail.com>
2025-05-13 17:35:02 +0200

    vaadin-14:fix: continue..

--- VaadinFlowStartup-14/src/main/java/com/infoworks/lab/domain/beans/tasks/rest/DownloadTask.java
+++ VaadinFlowStartup-14/src/main/java/com/infoworks/lab/domain/beans/tasks/rest/DownloadTask.java
@@ -3,11 +3,13 @@ package com.infoworks.lab.domain.beans.tasks.rest;
 import com.infoworks.lab.beans.tasks.rest.client.spring.methods.GetTask;
 import com.infoworks.lab.rest.models.Message;
 import com.infoworks.lab.rest.models.Response;
+import com.infoworks.lab.util.services.iResourceService;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Base64;
 import java.util.function.Consumer;
 
 public class DownloadTask extends GetTask {
@@ -31,8 +33,16 @@ public class DownloadTask extends GetTask {
                     , getBody()
                     , Resource.class
                     , getParams());
-            if (getResponseListener() != null)
-                getResponseListener().accept(null);
+            if (getResponseListener() != null) {
+                String base64Encoded = null;
+                if (response != null && response.getBody() != null) {
+                    Resource resource = response.getBody();
+                    iResourceService service = iResourceService.create();
+                    byte[] bytes = service.readAsBytes(resource.getInputStream());
+                    base64Encoded = new String(Base64.getEncoder().encode(bytes), "UTF-8");
+                }
+                getResponseListener().accept(base64Encoded);
+            }
             return (ResourceResponse) new ResourceResponse()
                     .setResource(response.getBody())
                     .setStatus(200)
@@ -44,6 +54,21 @@ public class DownloadTask extends GetTask {
                     .setMessage(getUri())
                     .setError(e.getMessage());
         }
+    }
+
+    protected String getUri() {
+        StringBuilder builder = new StringBuilder();
+        if (this.baseUri != null && !this.baseUri.isEmpty()) {
+            builder.append(this.baseUri.endsWith("/")
+                    ? this.baseUri.substring(0, (this.baseUri.length() - 1))
+                    : this.baseUri);
+        }
+        if (this.requestUri != null && !this.requestUri.isEmpty()) {
+            builder.append(this.requestUri.startsWith("/")
+                    ? this.requestUri
+                    : "/" + this.requestUri);
+        }
+        return builder.toString();
     }
 
     public static class ResourceResponse extends Response {
