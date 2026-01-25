eb133bbb199c8605d91dc9416824c201a6242fd4
m.towhid.islam <m.towhid.islam@gmail.com>
2026-01-25 19:15:57 +0100

    vaadin-24:fix: TabView Updated

--- VaadinFlowSpringStartup-24/src/main/java/com/infoworks/components/component/TabView.java
+++ VaadinFlowSpringStartup-24/src/main/java/com/infoworks/components/component/TabView.java
@@ -13,20 +13,41 @@ import java.util.Map;
 
 public class TabView extends Composite<Div> {
 
-    private Map<Tab, Component> tab2Workspace = new HashMap<>();
+    private TabVariant[] themeVariants = { TabVariant.LUMO_ICON_ON_TOP };
+    private final Map<Tab, Component> tab2Contents = new HashMap<>();
+    private Tab[] orderedTabs;
 
     public TabView() {}
 
-    public TabView(Map<Tab, Component> tab2Workspace) {
-        this.tab2Workspace = tab2Workspace;
+    public TabView(Tab[] tabs, Component[] components) {
+        setTab2Contents(tabs, components);
     }
 
-    public Map<Tab, Component> getTab2Workspace() {
-        return tab2Workspace;
+    public TabView(Map<Tab, Component> tab2Contents) {
+        setTab2Contents(tab2Contents.keySet().toArray(new Tab[0])
+                , tab2Contents.values().toArray(new Component[0]));
     }
 
-    public void setTab2Workspace(Map<Tab, Component> tab2Workspace) {
-        this.tab2Workspace = tab2Workspace;
+    public Tab[] getTabs() {
+        return orderedTabs;
+    }
+
+    public void setTab2Contents(Tab[] tabs, Component[] components) {
+        if (tabs.length != components.length)
+            throw new RuntimeException("TabView: Inconsistent Argument Exceptions. Tab[] tabs and Component[] components length are not equal!");
+        this.orderedTabs = tabs;
+        int index = 0;
+        for (Tab tab : orderedTabs) {
+            this.tab2Contents.put(tab, components[index++]);
+        }
+    }
+
+    public void setThemeVariants(TabVariant[] themeVariants) {
+        this.themeVariants = themeVariants;
+    }
+
+    public TabVariant[] getThemeVariants() {
+        return themeVariants;
     }
 
     @Override
@@ -41,25 +62,25 @@ public class TabView extends Composite<Div> {
         tabContentLayout.setSizeFull();
 
         //Tabs:-
-        Tab[] tabList = tab2Workspace.keySet().toArray(new Tab[0]);
+        Tab[] tabItems = getTabs();
 
         //Set the icon on top:-
-        for (Tab tab : tabList) {
-            tab.addThemeVariants(TabVariant.LUMO_ICON_ON_TOP);
+        for (Tab tab : tabItems) {
+            tab.addThemeVariants(getThemeVariants());
         }
 
         //Tabs config:-
-        Tabs tabs = new Tabs(tabList);
+        Tabs tabs = new Tabs(tabItems);
         tabs.setWidthFull();
         tabs.addSelectedChangeListener(event -> {
             final Tab selectedTab = event.getSelectedTab();
-            final Component component = tab2Workspace.get(selectedTab);
+            final Component component = tab2Contents.get(selectedTab);
             tabContentLayout.removeAll();
             tabContentLayout.add(component);
         });
 
         //Set default content:-
-        if(tabList.length > 0) tabContentLayout.add(tab2Workspace.get(tabList[0]));
+        if(tabItems.length > 0) tabContentLayout.add(tab2Contents.get(tabItems[0]));
 
         //Set init tab-content:-
         getContent().add(tabs, tabContentLayout);
