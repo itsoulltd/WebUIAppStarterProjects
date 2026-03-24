29b4e3aea37cf6a8899dd809ad0c665cb7e78cc2
m.towhid.islam <m.towhid.islam@gmail.com>
2026-03-25 00:11:20 +0100

    react:feat: adding read/iterate/readFromBinary to Excel

--- ReactAppStarter/src/Components/Utils/Excel.ts
+++ ReactAppStarter/src/Components/Utils/Excel.ts
@@ -1,10 +1,20 @@
-import { utils, WorkBook, WorkSheet, write, writeFile } from "xlsx"
+import {Sheet2JSONOpts, utils, WorkBook, WorkSheet, read, write, writeFile} from "xlsx"
 
 export interface ExcelRow {
     [key: string] : string | number | boolean;
 }
 
 export const Excel = {
+    read: (sheet: WorkSheet, option: Sheet2JSONOpts = { defval: "" }): ExcelRow[] => {
+        // convert to JSON
+        const rows = utils.sheet_to_json<ExcelRow>(sheet, option);
+        return rows;
+    },
+    iterate: (workbook: WorkBook): WorkSheet[] => {
+        const sheets: WorkSheet[] = [];
+        workbook.SheetNames.map((name) => sheets.push(workbook.Sheets[name]))
+        return sheets;
+    },
     worksheet: (rows: ExcelRow[]): WorkSheet | null => {
         if (!rows.length) return null;
         // headers from keys:
@@ -23,6 +33,11 @@ export const Excel = {
     },
     append: (workbook: WorkBook, worksheet: WorkSheet, sheetName: string): void => {
         utils.book_append_sheet(workbook, worksheet, sheetName);
+    },
+    readFromBinary: (binary: string | ArrayBuffer): WorkBook => {
+        return (binary instanceof ArrayBuffer)
+            ? read(binary, { type: "array" })
+            : read(binary, { type: "binary" });
     },
     writeToBinary: (workbook: WorkBook): Blob => {
         // create blob:
